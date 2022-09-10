ba04ba82bec0b29e3fd9ac7c232f6b87dda5f196
The Vinh <89290241+ntvinh2005@users.noreply.github.com>
2022-09-11 00:39:15 +0700

    Add new server

--- src/contexts/constants.js
+++ src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
-		? 'http://localhost:5000/api'
-		: ''
+		? 'https://young-taiga-80393.herokuapp.com/api'
+		: 'http://localhost:5000/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'vinh-yeah'
