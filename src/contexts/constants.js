e1582612383671d59de268b94fbd1666768bb893
The Vinh <89290241+ntvinh2005@users.noreply.github.com>
2022-09-11 15:36:55 +0700

    Fixed link

--- src/contexts/constants.js
+++ src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
-		? 'https://young-taiga-80393.herokuapp.com/api'
-		: 'http://localhost:5000/api'
+		? 'http://localhost:5000/api'
+		: 'https://young-taiga-80393.herokuapp.com/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'vinh-yeah'
