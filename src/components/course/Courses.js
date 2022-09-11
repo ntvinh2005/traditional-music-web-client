317f5c21c60e64fb4428463c6e375d41c59becee
The Vinh <89290241+ntvinh2005@users.noreply.github.com>
2022-09-11 10:37:42 +0700

    Fix link

--- src/components/course/Courses.js
+++ src/components/course/Courses.js
@@ -11,7 +11,7 @@ const Courses = ({ courses }) => {
             className="card m-5 bg-yellow-100 hover:bg-yellow-200 border-2 border-transparent hover:border-yellow-800"
           >
             <a
-              href={"http://localhost:3000/course/" + course._id}
+              href={"/course/" + course._id}
               className="text-yellow-600 hover:text-yellow-700"
             >
               {course.title}
