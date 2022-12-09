b4b8949c29e425e1ee648cf3e97debd0f791e1a9
The Vinh <89290241+ntvinh2005@users.noreply.github.com>
2022-12-09 16:00:51 +0700

    Add new server

--- src/contexts/constants.js
+++ src/contexts/constants.js
@@ -1,6 +1,8 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
 		? 'http://localhost:5000/api'
-		: 'https://young-taiga-80393.herokuapp.com/api'
+		: 'https://traditional-music-web-server.onrender.com/api'
 
+//'http://localhost:5000/api'
+//'https://traditional-music-web-server.onrender.com/api'
 export const LOCAL_STORAGE_TOKEN_NAME = 'vinh-yeah'
